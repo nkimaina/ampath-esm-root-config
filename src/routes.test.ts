0b9ee6f667203a8fad2df6de212fbed4be1669ec
Nicky Kibor Kimaina <nkimaina@ampath.or.ke>
2019-12-16 17:20:55 +0300

    Added ability to load angular form entry (#8)

--- src/routes.test.ts
+++ src/routes.test.ts
@@ -7,12 +7,44 @@ describe("routes", () => {
     ).toBe(false);
     expect(
       activityFunctions.ampathApps["@ampath/poc"](
+        mockLocation("/amrs/spa/patient")
+      )
+    ).toBe(false);
+    expect(
+      activityFunctions.ampathApps["@ampath/poc"](
+        mockLocation("/amrs/spa/home")
+      )
+    ).toBe(false);
+    expect(
+      activityFunctions.ampathApps["@ampath/poc"](
         mockLocation("/something-random")
       )
     ).toBe(true);
     expect(
       activityFunctions.ampathApps["@ampath/poc"](
         mockLocation("/amrs/spa/something-random")
+      )
+    ).toBe(true);
+  });
+  it(`returns correct values for @ampath/esm-form-entry routes`, () => {
+    expect(
+      activityFunctions.ampathApps["@ampath/esm-angular-form-entry"](
+        mockLocation("#/login")
+      )
+    ).toBe(false);
+    expect(
+      activityFunctions.ampathApps["@ampath/esm-angular-form-entry"](
+        mockLocation("/something-random")
+      )
+    ).toBe(false);
+    expect(
+      activityFunctions.ampathApps["@ampath/esm-angular-form-entry"](
+        mockLocation("/chart/formentry")
+      )
+    ).toBe(true);
+    expect(
+      activityFunctions.ampathApps["@ampath/esm-angular-form-entry"](
+        mockLocation("patient/some-long-uuid/chart/formentry")
       )
     ).toBe(true);
   });
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,25 @@
 export const ampathApps = {
-  "@ampath/poc": isPocActive
+  "@ampath/poc": isPocActive,
+  "@ampath/esm-angular-form-entry": shouldShowPatientChartFormEntry
 };
 // Read more about "activity functions" at https://single-spa.js.org/docs/configuration.html#activity-function
 function isPocActive(location: Location): boolean {
   // To start out, POC will always be mounted. This will change as more applications
   // are added.
-  return !location.pathname.includes("login");
+  return !location.pathname.includes("login") && !shouldNotLoadPOC(location);
+}
+
+function shouldShowPatientChartFormEntry(location: Location): boolean {
+  return (
+    !location.pathname.includes("login") &&
+    location.pathname.includes("/chart/formentry")
+  );
+}
+
+function shouldNotLoadPOC(location: Location): boolean {
+  return (
+    location.pathname.includes("/spa/patient") ||
+    location.pathname.includes("/spa/home") ||
+    location.pathname.includes("/chart/formentry")
+  );
 }
