792a5c6d3e740d8886c4331b9977b5bf3dc3a19c
Nicky Kibor Kimaina <nkimaina@ampath.or.ke>
2020-04-03 12:08:53 +0300

    Added program forms config (#24)

--- src/root-config.ts
+++ src/root-config.ts
@@ -6,9 +6,11 @@ import { provide } from "@openmrs/esm-module-config";
 //@ts-ignore
 import ampathLoginConfig from "./configs/login.config.json";
 import ampathPatientChartConfig from "./configs/patient-chart.config.json";
+import ampathPatientChartWidgetsConfig from "./configs/patient-chart-widgets.config.json";
 
 provide(ampathLoginConfig);
 provide(ampathPatientChartConfig);
+provide(ampathPatientChartWidgetsConfig);
 registerAllCoreApplications();
 registerAmpathApplications();
 
